2f37a1987df0c35daec31cdc3bc523b42bb0865d
Thiago <thiagodiasbispo@gmail.com>
2020-08-02 21:23:03 -0300

    Nova versão de src/02_clusterizar_lazy_version.exportado_automaticamente.py

--- 02_clusterizar_lazy_version.sh
+++ 02_clusterizar_lazy_version.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 NB_NAME=02_clusterizar_lazy_version.ipynb
-SCRIPT_NAME=02_clusterizar_lazy_version.automatically_exported
+SCRIPT_NAME=02_clusterizar_lazy_version.exportado_automaticamente
 
 jupyter nbconvert --output-dir src --output $SCRIPT_NAME --to script $NB_NAME
+
+git add src/*
+git commit -m "Nova versão de src/$SCRIPT_NAME.py"
 
 python src/$SCRIPT_NAME.py data
